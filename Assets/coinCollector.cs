5f55fd42289b205b897599b82d2712edcf582085
simplisticmartin <simplisticmartin@gmail.com>
2024-02-13 21:20:49 -0500

    add coin collecting scripts

---
+++ Assets/coinCollector.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinCollector : MonoBehaviour
+{
+    public bool hasCoin = false;
+    private int count;
+    public TextMeshProUGUI countText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+
+    }
+    void SetCountText()
+    {
+        countText.text = "" + count.ToString();
+    }
+        public void ReceiveCoin() { hasCoin = true;
+    }
+}
