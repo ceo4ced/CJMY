7ccc069be9cd7d5fdd8ab4958c8f941e06f8aee2
Jon Doerr <jondoerr21@gmail.com>
2024-04-14 21:43:51 -0400

    add manual and pause menu

--- Assets/Scripts/PauseMenuToggle.cs
+++ Assets/Scripts/PauseMenuToggle.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-[RequireComponent(typeof(CanvasGroup))]
 public class PauseMenuToggle : MonoBehaviour
 {
 
-    private CanvasGroup canvasGroup;
+    public CanvasGroup canvasGroup;
+    public GameObject pauseMenu;
+    private bool paused;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,16 +19,44 @@ public class PauseMenuToggle : MonoBehaviour
         canvasGroup = GetComponent<CanvasGroup>();
     }
 
+    public void Unpause()
+    {
+        pauseMenu.SetActive(false);
+        paused = false;
+        canvasGroup.interactable = false; canvasGroup.blocksRaycasts = false; canvasGroup.alpha = 0f; Time.timeScale = 1f;
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        paused = true;
+        canvasGroup.interactable = true; canvasGroup.blocksRaycasts = true; canvasGroup.alpha = 1f; Time.timeScale = 0f;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false; // Uncomment if you want to use it in the editor
+    }
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("TESTCREATENEWSCENE");
+        Unpause();
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyUp(KeyCode.Escape))
         {
-            if (canvasGroup.interactable)
+            if (paused)
             {
-                canvasGroup.interactable = false; canvasGroup.blocksRaycasts = false; canvasGroup.alpha = 0f; Time.timeScale = 1f;
+                Unpause();
             }
-            else { canvasGroup.interactable = true; canvasGroup.blocksRaycasts = true; canvasGroup.alpha = 1f; Time.timeScale = 0f; }
+            else
+            {
+                Pause();
+            }
         }
 
     }
